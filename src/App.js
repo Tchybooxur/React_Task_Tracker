58a97822f93aace06c2db24c09bf06b4419a1c91
Eze Chibuzor <buzoreze12@hotmail.com>
2022-06-14 22:25:15 +0100

    modified Button & Header; added Task & Tasks components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id : 1,
+        text : "Doctor's appointment",
+        day : "Feb 5th at 02:30pm",
+        reminder : true,
+    },
+    {
+        id : 2,
+        text : "Meeting at school",
+        day : "Feb 6th at 01:30pm",
+        reminder : true,
+    },
+    {
+        id : 3,
+        text : "Food shopping",
+        day : "Feb 5th at 02:30pm",
+        reminder : false,
+    },
+])
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={ tasks }/>
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,17 @@
 import PropTypes from 'prop-types'
 
-const Button = ({ color, text }) => {
-  return <button style={{ backgroundColor : color }} className='btn'>{text}</button>
+const Button = ({ color, text, onClick }) => {
+  return <button style={{ backgroundColor : color }} 
+  className='btn' onClick={onClick}>{text}</button>
 }
 
 Button.defaultProps = {
-    color : 'steelblue'
+    color : 'steelblue',
 }
 
 Button.propTypes = {
     text : PropTypes.string,
-    color : PropTypes.string
+    color : PropTypes.string,
+    onClick : PropTypes.func
 }
 export default Button
